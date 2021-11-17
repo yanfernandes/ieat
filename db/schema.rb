0115364ceac3e83a3ec8ce51ee03463242b2ec83
Yan <yanfernandes@gmail.com>
2021-11-17 20:30:14 -0300

    add restaurant and product models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_202436) do
+ActiveRecord::Schema.define(version: 2021_11_17_232657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "value"
+    t.string "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
